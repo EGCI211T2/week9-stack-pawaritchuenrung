916dbf294502c0994fad49cf173ddefcd3ef6269
pawaritchuenrung <pawaritchuenrung@gmail.com>
2026-03-13 04:02:57 +0000

    f

--- main2.cpp
+++ main2.cpp
@@ -3,35 +3,38 @@ using namespace std;
 
 #include "stack.h"
 
-int main(int argc, char **argv){
-    Stack s;
-   
+int main(int argc, char *argv[]) {
 
-  //Exercise 2
- printf("Checking the parentheses in argv arguments\n");
+    for(int i=1;i<argc;i++){
 
-  for(i=0;i<argc;i++){
-     for(j=0;j<strlen(argv[i]);j++){
-       //Use stack to help with the parentheses
-      
-      
+        Stack s;
+        bool correct = true;
 
-  switch(argv[i][j])
-  {
-    case '[' :
-    case '(' :
-    case '{' : s.push(argv[i][j]); break;
-    case '}' : t=s.pop();
-      if(t!='{') match=0;
+        for(int j=0; argv[i][j] != '\0'; j++){
 
-  }
+            if(argv[i][j]=='[' || argv[i][j]=='{'){
+                s.push(argv[i][j]);
+            }
 
+            else if(argv[i][j]==']' || argv[i][j]=='}'){
 
-      break;
+                char x = s.pop();
 
+                if((argv[i][j]==']' && x!='[') ||
+                   (argv[i][j]=='}' && x!='{')){
+                    correct = false;
+                    break;
+                }
+            }
         }
-      }
-    }
 
-   return 0;
+        if(!correct)
+            cout<<"argv "<<i<<" incorrect"<<endl;
+
+        else if(!s.isEmpty())
+            cout<<"argv "<<i<<" incorrect: too many open parenthesis"<<endl;
+
+        else
+            cout<<"argv "<<i<<" correct"<<endl;
+    }
 }
--- stack.h
+++ stack.h
@@ -3,44 +3,65 @@
 #define stack_h
 #include "node.h"
 class Stack {
-	NodePtr top;
-	int size;
+    NodePtr top;
+    int size;
 public:
-    void push(int);
-    int pop();
+    void push(char);
+    char pop();
     Stack();
     ~Stack();
-    bool isEmpty(){ return size==0; }
+    bool isEmpty();
 };
 
 
-void Stack::push(int x){
+void Stack::push(char x){
   NodePtr new_node=new NODE(x);
   if(new_node){
-            // Left missing for exercises…
+            new_node->set_next(top);
+            top = new_node;
+            size++;
    }
- 
-         // Left missing for exercises…
-    
+         // change the top
 }
 
-int Stack::pop(){
+char Stack::pop(){
+    if(size>0){
         NodePtr t=top;
-        int value;
+        char value;
         value=t->get_value();
+        top = t->get_next();
+
+   
+       
     // Left missing part for exercises
         delete t;
+        size--;
         return value;
-	//be careful of the empty stack!!!
     }
+
+    //be careful of the empty stack!!!
+    else{
+        return '\0';
+    }
+    }
+
+bool Stack::isEmpty(){
+    return size == 0;
+}
 
 Stack::Stack(){
     //initialize stack
-    
+    top = NULL;
+    size = 0;
+   
 }
 Stack::~Stack(){
-    //delete all remaning stack (i.e. pop all) 
-    
+    //delete all remaning stack (i.e. pop all)
+    while(size>0) {
+    pop();
+    }
+
+   
 }
 
 
